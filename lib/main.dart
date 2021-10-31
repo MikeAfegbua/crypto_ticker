197cc35fb00e594cb903433da982827e569ef0fe
Micheal <aokhaimoh@yahoo.com>
2021-10-31 17:41:05 +0100

    complete UI

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'price_screen.dart';
+
+void main() => runApp(MyApp());
+
+// ignore: use_key_in_widget_constructors
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+          primaryColor: Colors.lightBlue,
+          appBarTheme: const AppBarTheme(
+            color: Colors.lightBlue,
+          ),
+          scaffoldBackgroundColor: Colors.white),
+      home: PriceScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
---
+++ lib/price_screen.dart
@@ -0,0 +1,101 @@
+// ignore_for_file: avoid_print
+
+import 'package:flutter/material.dart';
+import 'coin_data.dart';
+import 'package:flutter/cupertino.dart';
+import 'dart:io' show Platform;
+
+// ignore: use_key_in_widget_constructors
+class PriceScreen extends StatefulWidget {
+  @override
+  _PriceScreenState createState() => _PriceScreenState();
+}
+
+class _PriceScreenState extends State<PriceScreen> {
+  String selectedCurrency = 'USD';
+  // FUNCTION FOR ANDROID PICKER
+
+  DropdownButton<String> androidDropdown() {
+    List<DropdownMenuItem<String>> dropdownItems = [];
+    for (String currency in currenciesList) {
+      dropdownItems.add(
+        DropdownMenuItem(
+          child: Text(currency),
+          value: currency,
+        ),
+      );
+    }
+    return DropdownButton<String>(
+      value: selectedCurrency,
+      items: dropdownItems,
+      onChanged: (value) {
+        setState(
+          () {
+            selectedCurrency = value.toString();
+          },
+        );
+      },
+    );
+  }
+  // FUNCTION FOR IOS PICKER
+
+  CupertinoPicker iOSPicker() {
+    List<Text> textList = [];
+    for (String currency in currenciesList) {
+      textList.add(
+        Text(currency),
+      );
+    }
+    return CupertinoPicker(
+      backgroundColor: Colors.lightBlue,
+      itemExtent: 32.0,
+      onSelectedItemChanged: (selectedItem) {
+        print(selectedItem);
+      },
+      children: textList,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('🤑 Coin Ticker'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+            child: Card(
+              color: Colors.lightBlueAccent,
+              elevation: 5.0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: const Padding(
+                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+                child: Text(
+                  '1 BTC = ? USD',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            height: 150.0,
+            alignment: Alignment.center,
+            padding: const EdgeInsets.only(bottom: 30.0),
+            color: Colors.lightBlue,
+            child: Platform.isIOS ? iOSPicker() : androidDropdown(),
+          ),
+        ],
+      ),
+    );
+  }
+}
